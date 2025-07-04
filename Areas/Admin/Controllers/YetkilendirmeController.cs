077029c0c2aabce3b1ef5f55f7f95fd6fab063d6
Berke Küçük <brke.kucuk.22@gmail.com>
2025-07-04 15:18:19 +0300

    Auth Session

--- Areas/Admin/Controllers/YetkilendirmeController.cs
+++ Areas/Admin/Controllers/YetkilendirmeController.cs
@@ -1,24 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Otobur.DataAccess.Data;
-using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Otobur.DataAccess.Repository.IRepository;
 
 namespace Otobur.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class YetkilendirmeController : Controller
     {
-        private readonly ApplicationDbContext _db;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public YetkilendirmeController(ApplicationDbContext db)
+        public YetkilendirmeController(IUnitOfWork unitOfWork)
         {
-            _db = db;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            var kullanicilar = _db.Kullanicilar.ToList();
+            var kullaniciGrubu = HttpContext.Session.GetString("KullaniciGrubu");
+            if (kullaniciGrubu != "Sistem Yöneticisi")
+            {
+                return RedirectToAction("Login", "Account", new { area = "Kullanici" });
+            }
+            var kullanicilar = _unitOfWork.Kullanici.GetAll().ToList();
             return View(kullanicilar);
         }
-      
+
+        // Diğer action'larda da aynı kontrolü yapın
     }
 }
